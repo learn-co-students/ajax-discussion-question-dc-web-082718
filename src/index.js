8331f719f4e82dc6f5a54acb5f25eeed18c1c25b
patti-c <patricia.carter@flatironschool.com>
2018-10-15 09:31:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  console.log("Hello")
+
+  fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then( data => {
+      console.log("Hi")
+    })
+
+  console.log("Sup?")
+
+  function fetchData1(){
+          var data = fetch('https://randomuser.me/api/')
+            .then( res => res.json() )
+            .then( json => console.log(json) )
+  }
+
+  fetchData1()
+
+  function fetchData2(){
+          var data = fetch('https://randomuser.me/api/')
+            .then( res => res.json() )
+            .then( console.log )
+  }
+
+  fetchData2()
+
 });
